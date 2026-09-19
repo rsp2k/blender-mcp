9d5bb01674e22f9d5fe22bca31d2a35b47b95790
Ryan Malloy <ryan@supported.systems>
2026-09-19 14:02:32 -0600

    chore(addon): bump 1.5.22 -> 1.5.23

--- addon/_version.py
+++ addon/_version.py
@@ -5,5 +5,5 @@ A simple grep test in CI (or a `make check-version` target) can confirm
 this string matches the rest of the project.
 """
 
-__version__ = "1.5.22"
+__version__ = "1.5.23"
 tuple_version = tuple(int(p) for p in __version__.split("."))
