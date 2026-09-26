f2491b874923260b1baf5596e3f5058497b84dab
Ryan Malloy <ryan@supported.systems>
2026-09-26 13:47:42 -0600

    chore: bump 2026.926.12 -> 2026.926.13

--- addon.py
+++ addon.py
@@ -21,7 +21,7 @@
 bl_info = {
     "name": "Blender MCP",
     "author": "BlenderMCP",
-    "version": (2026, 926, 12),  # MUST match addon/_version.py:tuple_version
+    "version": (2026, 926, 13),  # MUST match addon/_version.py:tuple_version
     "blender": (3, 2, 0),  # uses bpy.context.temp_override (3.2+)
     "location": "View3D > Sidebar > BlenderMCP",
     "description": (
