670dcbd096e13b79dc08b5c1c17b84322aa19206
Ryan Malloy <ryan@supported.systems>
2026-09-26 14:19:46 -0600

    chore: bump 2026.926.13 -> 2026.926.14

--- addon/_version.py
+++ addon/_version.py
@@ -5,5 +5,5 @@ A simple grep test in CI (or a `make check-version` target) can confirm
 this string matches the rest of the project.
 """
 
-__version__ = "2026.926.13"
+__version__ = "2026.926.14"
 tuple_version = tuple(int(p) for p in __version__.split("."))
